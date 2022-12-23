b61ed974d376c5b30e7e8a5f2015e39a4a0dad09
Zakhary Kaplan <me@zakhary.dev>
2022-12-23 14:30:00 -0500

    feat(server): improve routes

--- src/srv/route.rs
+++ src/srv/route.rs
@@ -18,7 +18,9 @@ pub async fn index() -> impl IntoResponse {
 
 pub async fn login(auth: AuthContext) -> impl IntoResponse {
     match auth.current_user {
+        // Redirect if already logged in
         Some(_) => Redirect::to("/rsvp").into_response(),
+        // Perform the login
         None => Login::get().await.into_response(),
     }
 }
@@ -29,6 +31,7 @@ pub async fn auth(
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
     Form(mut user): Form<User>,
 ) -> impl IntoResponse {
+    // Query the database using provided credentials
     trace!("attempt: `{user}`, from: {addr}");
     let Some(ident) = db.query(&user) else {
         // User not found
@@ -40,12 +43,14 @@ pub async fn auth(
     user.ident = ident;
     // Authenticate user
     auth::login(auth, user).await;
-    // Redirect onwards to rsvp
+    // Redirect onwards to RSVP
     Redirect::to("/rsvp")
 }
 
 pub async fn logout(auth: AuthContext) -> impl IntoResponse {
+    // Perform logout for the user
     auth::logout(auth).await;
+    // Redirect to the homepage
     Redirect::to("/")
 }
 
@@ -54,7 +59,12 @@ pub async fn registry() -> impl IntoResponse {
 }
 
 pub async fn rsvp(auth: AuthContext) -> impl IntoResponse {
-    Rsvp::get(auth.current_user.unwrap().clone()).await
+    // Redirect to the login if no user authenticated
+    let Some(user) = auth.current_user.clone() else {
+        return Redirect::to("/login").into_response();
+    };
+    // Present RSVP form
+    Rsvp::get(user).await.into_response()
 }
 
 pub async fn reply(
@@ -62,13 +72,21 @@ pub async fn reply(
     auth: AuthContext,
     Form(reply): Form<Reply>,
 ) -> impl IntoResponse {
-    let user = auth.current_user.unwrap();
+    // Do nothing if not logged in
+    let Some(user) = auth.current_user else {
+        // User not found, return status code
+        return StatusCode::UNAUTHORIZED.into_response();
+    };
+    // Update this user's reply
     db.update(&user, reply).unwrap();
+    // Save the database to a file (optional)
+    // TODO: Should this be done async?
     match db.write() {
         Ok(_) | Err(db::Error::Path) => (),
         Err(err) => error!("{err}"),
     };
-    Redirect::to("/")
+    // Redirect to the homepage
+    Redirect::to("/").into_response()
 }
 
 #[derive(Template)]
