473b16d5264a390f29a987d2725e2c4888b34075
Zakhary Kaplan <me@zakhary.dev>
2023-01-11 13:45:00 -0500

    feat(www): update days.js

--- www/script/days.js
+++ www/script/days.js
@@ -1,23 +1,29 @@
 const pluralize = (count, noun, suffix = "s") =>
   `${count} ${noun}${count !== 1 ? suffix : ""}`;
 
-function days(deadline) {
+function countdown(deadline) {
     const now   = new Date();
     const total = deadline - now;
-    const duration = {
+    return {
         total,
         days:    Math.floor( total / (1000 * 60 * 60 * 24)),
         hours:   Math.floor((total /  1000  / 60) % 60),
         minutes: Math.floor((total / (1000  * 60 *  60)) % 24),
         seconds: Math.floor((total /  1000) % 60),
-    };
+    }
+}
 
-    const days = document.getElementById("days");
-    days.innerHTML = pluralize(duration.days, "day");
+function update(elem, deadline) {
+    // Calculate the countdown
+    const dur = countdown(deadline);
+    const days = Math.max(0, dur.days + 1);
+    // Update the element
+    elem.innerHTML = pluralize(days, "day");
 }
 
 // Mark the deadline
 const deadline = new Date("2023-06-06 00:00:00");
+const elem = document.getElementById("days");
 
-// Set the initial tick
-days(deadline);
+// Set the initial countdown
+update(elem, deadline);
